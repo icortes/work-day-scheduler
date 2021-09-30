cd03b3c7c2054b571d196db6c0d4759975952104
isa <5445946+icortes@users.noreply.github.com>
2021-09-29 20:46:28 -0700

    made nine to five rows and wip color change based on current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     console.log(moment());
     console.log(moment().format());
 
     //constantly update current time
-    var updateDate = function(){
+    var updateDate = function () {
         var currentDay = moment().format('MMMM Qo, YYYY');
         $('#currentDay').text(currentDay);
     }
     //initially set date
     updateDate();
-    
+
     //timeblock class
-    class timeBlock{
-        constructor(time){
+    class timeBlock {
+        //massage to be stored
+        message="";
+        constructor(time) {
             this.time = time;
         }
-
-        get getTime(){
-            return this.time;
+        //sets message to the object
+        set setMessage(message){
+            this.message = message;
         }
         //will make a new row for the table
-        makeTimeBlock(){
-            
+        makeTimeBlock() {
+
             //make a new <div>
             var row = $('<div>');
             //make column for time
@@ -36,9 +38,9 @@ $(document).ready(function() {
             row.addClass("row");
             //add classes to hour
             hour.addClass("col col-1 hour");
-             //add classes to textarea
-             textArea.addClass('col col-10 description');
-             //add classes to the button
+            //add classes to textarea
+            textArea.addClass('col col-10 description');
+            //add classes to the button
             saveBtn.addClass('col col-1 saveBtn');
 
             //add time in the hour column
@@ -54,10 +56,34 @@ $(document).ready(function() {
             //append row to container
             $('.container').append(row);
         }
+
+        //method to color the timeblocks appropriately
+        #colorRow(row){
+            //fetch current hour
+            var currentHour = moment().hour();
+        }
+
     }
     //create timeblocks
-    var nineAM = new timeBlock("9 AM");
-    console.log(nineAM);
-    
-    nineAM.makeTimeBlock();
+    var nine = new timeBlock("9 AM");
+    var ten = new timeBlock("10 AM");
+    var eleven = new timeBlock("11 AM");
+    var twelve = new timeBlock("12 PM");
+    var one = new timeBlock("1 PM");
+    var two = new timeBlock("2 PM");
+    var three = new timeBlock("3 PM");
+    var four = new timeBlock("4 PM");
+    var five = new timeBlock("5 PM")
+    console.log(nine);
+
+    //call makeTimeBLock() to each object
+    nine.makeTimeBlock();
+    ten.makeTimeBlock();
+    eleven.makeTimeBlock();
+    twelve.makeTimeBlock();
+    one.makeTimeBlock();
+    two.makeTimeBlock();
+    three.makeTimeBlock();
+    four.makeTimeBlock();
+    five.makeTimeBlock();
 });
